304a0cb8bfc35f3605979c24ffff8b342f2455a8
vietnguyen-dev <vietnguyent22@gmail.com>
2026-08-16 18:42:07 -0700

    updated theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,19 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "storm",
-      transparent = true,
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight")
-    end,
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    variant = "default",
+    transparent = true,
+    terminal_colors = true,
   },
+  config = function(_, opts)
+    vim.opt.termguicolors = true
+    vim.opt.background = "dark"
+    vim.opt.winblend = 10
+    vim.opt.pumblend = 10
+
+    require("cyberdream").setup(opts)
+    vim.cmd.colorscheme("cyberdream")
+  end,
 }
